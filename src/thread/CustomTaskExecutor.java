74197aa57250be77dfc548ea90af930d985addd6
DhruvPa <E01612@hsc.com>
2020-01-20 01:22:39 +0530

    commiting code

---
+++ src/thread/CustomTaskExecutor.java
@@ -0,0 +1,55 @@
+package thread;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import database.DataSource;
+import database.ExecuteStatements;
+import exception.CustomException;
+import file.FileOperations;
+
+public class CustomTaskExecutor implements Runnable {
+
+	private Long position;
+	private FileOperations fo;
+	private Connection connection;
+	private ExecuteStatements exec;
+
+	public CustomTaskExecutor(Long position) {
+		this.position = position;
+	}
+
+	@Override
+	public void run() {
+		try {
+			executeTask();
+
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} catch (CustomException e) {
+			e.printStackTrace();
+		}
+	}
+
+	private synchronized void executeTask() throws ClassNotFoundException, SQLException, CustomException {
+		connection = DataSource.getConnection();
+		fo = FileOperations.FileOperations(
+				"C:\\\\Users\\\\e01612\\\\Desktop\\\\interview\\\\us-accidents\\\\US_Accidents_May19.csv");
+		exec = ExecuteStatements.ExecuteStatements();
+
+		try {
+
+			if (!exec.checkTableExists(connection, "analysis")) {
+				String val = "field1,field2";
+				exec.createTable(connection,"analysis", val);
+				DataSource.returnConnection(connection);
+			} else {
+				DataSource.returnConnection(connection);
+			}
+		} finally {
+			DataSource.returnConnection(connection);
+		}
+	}
+}
